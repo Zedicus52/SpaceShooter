c0f3f1ab5e992a0ff8e93ba85886a89902737903
Olexander Tsyganok <zedicusgd@gmail.com>
2023-01-14 10:40:40 +0200

    add spawners controller

---
+++ Assets/Scripts/Core/EnemySpawnerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using SpaceShooter.Abstraction;
+using UnityEngine;
+
+namespace SpaceShooter.Core
+{
+    [RequireComponent(typeof(EnemySpawner))]
+    public class EnemySpawnerController : MonoBehaviour
+    {
+        [SerializeField] private float delayBeforeSpawningNewEnemyType;
+        private List<EnemySpawner> _spawners;
+
+        private void Awake()
+        {
+            _spawners = GetComponents<EnemySpawner>().ToList();
+            foreach (var spawner in _spawners)
+            {
+                spawner.enabled = false;
+            }
+
+            StartCoroutine(SpawnerInitialization());
+        }
+
+        private IEnumerator SpawnerInitialization()
+        {
+            foreach (var spawner in _spawners)
+            {
+                spawner.enabled = true;
+                yield return new WaitForSeconds(delayBeforeSpawningNewEnemyType);
+            }
+        }
+    }
+}
